1e0fb19137cb830d9df6ef161d357fc88916f3c2
superefosa <eo60@students.quantic.edu>
2025-11-29 16:15:39 +0100

    deploy

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -1,11 +1,23 @@
-describe('Shows Appeteasers', () => {
-  it('Connect to Dev Server', () => {
-    cy.visit('https://hangryhippo.quantic.host/');
+describe("Shows Appeteasers", () => {
+  it("Connects to Dev Server", () => {
+    cy.visit("https://hangryhippo.quantic.host/");
   });
-  it('selects Handhelds', () => {
-    //TODO: Add a test to check Handhelds
+
+  it("Selects Handhelds", () => {
+    cy.contains("Handhelds").click();
+    cy.contains("Cheese Burger");
+    cy.contains("Fajita Tacos");
   });
-  it('selects Appeteasers', () => {
-    //TODO: Add a test to check Appeteasers
+
+  it("Selects Appeteasers", () => {
+    cy.contains("Appeteasers").click();
+
+    // Example items to check
+    cy.contains("Tater Tots");
+    cy.contains("Buffalo Wings");
+
+    // Ensure Handhelds items are not visible
+    cy.contains("Cheese Burger").should("not.exist");
+    cy.contains("Fajita Tacos").should("not.exist");
   });
 });
